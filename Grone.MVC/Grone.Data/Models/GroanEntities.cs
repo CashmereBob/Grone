85a53baecc3e980c304d21fc7dfe3391cc1e3859
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-01-17 16:49:35 +0100

    fixed grone spelling

--- Grone.MVC/Grone.Data/Models/GroanEntities.cs
+++ Grone.MVC/Grone.Data/Models/GroanEntities.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Grone.Data.Models
 {
-    public class GroanEntities:DbContext
+    public class GroneEntities:DbContext
     {
         DbSet<CommentEntityModel> Comments { get; set; }
         DbSet<PostEntityModel> Posts { get; set; }
