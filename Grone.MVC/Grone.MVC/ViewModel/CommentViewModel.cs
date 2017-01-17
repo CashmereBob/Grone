7b8997136f450d0f9c2724ad7c4a850bcad683ba
LeonardoTouma <mr_milan1@hotmail.com>
2017-01-17 12:08:01 +0100

    Vy för CommentViewModel klar.

--- Grone.MVC/Grone.MVC/ViewModel/CommentViewModel.cs
+++ Grone.MVC/Grone.MVC/ViewModel/CommentViewModel.cs
@@ -7,5 +7,11 @@ namespace Grone.MVC.ViewModel
 {
     public class CommentViewModel
     {
+        public Guid id { get; set; }
+        public string Comment  { get; set; }
+        public DateTime Date { get; set; }
+        public Guid Kommentarid { get; set; }
+        public string Member { get; set; }
+        public string ImgSrc { get; set; }
     }
 }
