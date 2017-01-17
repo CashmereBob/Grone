a641a1c4f080a1965d481d51d135ac23f0420971
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-01-17 11:53:35 +0100

    small change

--- Grone.MVC/Grone.Data/IRepository/ICommentRepository.cs
+++ Grone.MVC/Grone.Data/IRepository/ICommentRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Grone.Data.IRepository
 {
-    interface ICommentRepository
+    interface ICommentRepository//repo
     {
         void AddOrUpdate();
         void Delete(Guid id);
