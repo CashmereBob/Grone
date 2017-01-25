03cbb2154143707119984eef01d9bd9119eff94c
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-01-25 11:15:15 +0100

    mapper class modeltoentity for user

---
+++ Grone.MVC/Grone.MVC/HelpClasses/EFMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Grone.Data.Models;
+using Grone.MVC.ViewModel;
+
+namespace Grone.MVC.HelpClasses
+{
+    public static class EFMapper
+    {
+        internal static UserEntityModel ModelToEntity(UserAddViewModel model)
+        {
+
+            var entity = new UserEntityModel()
+            {
+                entity.eMail = model.eMail,
+                entity.Fullname = model.Fullname,
+                entity.Password = model.Password,
+            };
+            return entity;
+        }
+    }
+}
